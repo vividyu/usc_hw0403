a5e95cc46ef8c764c2d9f0cdb024db5f3a417f1c
Guanyu Hou <10951700+vividyu@users.noreply.github.com>
2023-04-02 16:36:24 -0500

    solvable

--- src/App.js
+++ src/App.js
@@ -2,16 +2,49 @@ import React from "react";
 import "./App.css";
 import Board from "./Board";
 
+function isSolvable(arr) {
+  let inversions = 0;
+  const tiles = arr.filter((tile) => tile !== 0);
+
+  for (let i = 0; i < tiles.length - 1; i++) {
+    for (let j = i + 1; j < tiles.length; j++) {
+      if (tiles[i] > tiles[j]) {
+        inversions++;
+      }
+    }
+  }
+
+  const emptyRowFromBottom = 4 - Math.floor(arr.indexOf(0) / 4);
+
+  return (inversions % 2 === 0) === (emptyRowFromBottom % 2 === 1);
+}
+
+function generateRandomArray() {
+  const arr = Array.from({ length: 16 }, (_, i) => i);
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+}
+
+function generateSolvableArray() {
+  let arr;
+
+  do {
+    arr = generateRandomArray();
+  } while (!isSolvable(arr));
+
+  return arr;
+}
+
 function App() {
-  const initialConfiguration = [
-    1, 2, 3, 4,
-    5, 6, 7, 8,
-    9, 10, 11, 12,
-    13, 14, 15, 0,
-  ];
+  const initialConfiguration = generateSolvableArray();
 
   const onSolveCallback = () => {
-    console.log("Puzzle solved!");
+    alert("Puzzle solved!");
   };
 
   return (
